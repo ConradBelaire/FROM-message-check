996ad0596f528f7d86fe9724d5b723bdc5844f06
Conrad Belaire <cbelaire@gmail.com>
2022-05-17 16:55:15 -0400

    Filled templates
    Changed format for increased readability.
    Added regex for every single template in Elden Ring.

--- code/main.py
+++ code/main.py
@@ -1,7 +1,32 @@
 import re
 
-words = ["skeleton", "dog"]
-regex = [re.compile("^Seek ([\w\s]+)$"), re.compile("^([\w\s]+) ahead$")]
+words = [
+	"skeleton",
+	"enemy",
+	"weak foe",
+	"dog"]
+
+regex = [
+	re.compile("^Seek ([\w\s]+)$"),
+	re.compile("^([\w\s]+) ahead$"),
+	re.compile("^No ([\w\s]+) ahead$"),
+	re.compile("^Be wary of ([\w\s]+)$"),
+	re.compile("^Try ([\w\s]+)$"),
+	re.compile("^First off, ([\w\s]+)$"),
+	re.compile("^Still no ([\w\s]+)[.]{3}$"),
+	re.compile("^Why is it always ([\w\s]+)\?$"),
+	re.compile("^Didn't expect ([\w\s]+)[.]{3}$"),
+	re.compile("^Could this be a ([\w\s]+)\?$"),
+	re.compile("^Time for ([\w\s]+)$"),
+	re.compile("^Behold, ([\w\s]+)!$"),
+	re.compile("^Offer ([\w\s]+)$"),
+	re.compile("^Praise the ([\w\s]+)!$"),
+	re.compile("^([\w\s]+)!$"),
+	re.compile("^([\w\s]+)\?$"),
+	re.compile("^([\w\s]+)[.]{3}$"),
+	re.compile("^([\w\s]+)$"),
+	re.compile("^If only I had a ([\w\s]+)[.]{3}$"),
+	re.compile("^Likely ([\w\s]+)$")]
 
 print("Input:")
 val = input()
