29baee7bb0d8db1fc679b25547cdb86de915ef6e
Conrad Belaire <cbelaire@gmail.com>
2022-05-17 16:33:46 -0400

    Working matcher!
    With the very limited list of templates/words, it successfully matches and returns info.

--- code/main.py
+++ code/main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import re
+
+words = ["skeleton", "dog"]
+regex = [re.compile("^Seek ([\w\s]+)$"), re.compile("^([\w\s]+) ahead$")]
+
+print("Input:")
+val = input()
+
+for r in regex:
+	m = r.match(val)
+	if m:
+		if m.group(1) in words:
+			print("Match!")
+			print(m)
+			print(m.group(1))
